a2ca41e842be9e18cc6b779b3ac5ac073de9ebb6
Ray-Love <print@star-xn.xyz>
2026-01-18 12:09:29 +0800

    添加一些美化

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,23 @@
+<template>
+  <Giscus
+    repo="StarStudio-Inc/FurWiki"
+    repoId="R_kgDOQ3B6Bg"
+    categoryId="DIC_kwDOQ3B6Bs4C1GC0"
+    mapping="pathname"
+    strict="0"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    lang="zh-CN"
+    :theme="giscusTheme"
+  />
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue'
+import { computed } from 'vue'
+import { useData } from 'vitepress'
+
+const { isDark } = useData()
+const giscusTheme = computed(() => (isDark.value ? 'dark_tritanopia' : 'light_tritanopia'))
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,8 +3,11 @@ import DefaultTheme from 'vitepress/theme'
 import './style/index.scss'
 // 客户端：暗黑切换径向动画
 import './dark-transition'
+import MyLayout from './components/MyLayout.vue'
+
 
 export default {
   extends: DefaultTheme,
+  Layout: MyLayout,
 
 }
